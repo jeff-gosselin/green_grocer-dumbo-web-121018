d02b1e5cf79c9d33e9a175226fc180e7670bdd4b
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-12-01 07:44:19 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,40 @@
+require 'pry'
+
+# "AVOCADO W/COUPON" => {:price => 5.0, :clearance => true, :count => 1}
+
 def consolidate_cart(cart)
-  # code here
+  new_cart = {}
+
+  cart.each do |sku|
+    sku.each do |item, data|
+      new_cart[item] = data
+
+      if new_cart[item].has_key?(:count)
+        new_cart[item][:count] += 1 
+      else
+        new_cart[item][:count] = 1
+      end
+  
+    end
+  end
+  new_cart
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  cart.each do |item, info|
+    coupons.each do |coupon|
+        
+      if coupon[:item] == item              # Names coupon
+        cart["#{item} W/ COUPON"] = {}
+      end
+      
+      # cart[coupon_title] = {}
+      
+      
+      
+    end
+  end
+  
 end
 
 def apply_clearance(cart)
