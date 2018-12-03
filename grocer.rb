b0e45fb230bb5c2d87c9cc5087e6e3e51bd7ca94
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-12-03 03:51:38 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,10 +1,54 @@
+require 'pry'
+
+# "AVOCADO W/COUPON" => {:price => 5.0, :clearance => true, :count => 1}
+
 def consolidate_cart(cart)
-  # code here
+  new_cart = {}
+
+  cart.each do |sku|
+    sku.each do |item, data|
+      new_cart[item] = data
+
+      if new_cart[item].has_key?(:count)
+        new_cart[item][:count] += 1 
+      else
+        new_cart[item][:count] = 1
+      end
+  
+    end
+  end
+  new_cart
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  reduced = cart.dup
+  coupon_name = nil
+  
+  
+  cart.each do |item, info|
+    coupons.each do |coupon|
+      
+      cost = coupon[:cost] 
+      number = coupon[:num]
+      
+        
+      if coupon[:item] == item && reduced[item][:count] >= coupon[:num]  
+        coupon_name = "#{item} W/COUPON"
+        reduced[item][:count] -= coupon[:num] 
+      
+      end
+      
+      if reduced.key?(coupon_name) && reduced[item][:count] >= coupon[:num]
+        reduced[coupon_name][:count] +=1
+      else
+        reduced[coupon_name] = {price: cost, :clearance => info[:clearance], count: 1 }
+      end
+    end
+  end
+  reduced
 end
+
+
 
 def apply_clearance(cart)
   # code here
