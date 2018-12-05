dd6f87cdcfba07067013a5d3c692fa78e8a78dff
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-12-05 05:47:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -4,7 +4,6 @@ require 'pry'
 
 def consolidate_cart(cart)
   new_cart = {}
-
   cart.each do |sku|
     sku.each do |item, data|
       new_cart[item] = data
@@ -20,29 +19,65 @@ def consolidate_cart(cart)
   new_cart
 end
 
+# def apply_coupons(cart, coupons)
+#   the_discount = {}
+  
+#   cart.each do |item, info|
+#     coupons.each do |coupon|
+      
+    
+#       if coupon[:item] == item && !the_discount.key?("#{coupon[:item]} W/COUPON") && info[:count] >= coupon[:num]
+#         the_discount["#{coupon[:item]} W/COUPON"] = {:price => coupon[:cost], :count => 1, :clearance => info[:clearance]}
+        
+#         if coupon[:num] >= info[:count]
+#         qty = coupon[:num] - info[:count]
+#         cart[item][:count] = qty
+#       else
+#         qty = info[:count] - coupon[:num]
+#         cart[item][:count] = qty
+#       end
+        
+        
+#       elsif coupon[:item] == item && the_discount.key?("#{coupon[:item]} W/COUPON") && info[:count] >= coupon[:num]
+        
+#         the_discount["#{coupon[:item]} W/COUPON"][:count] += 1
+#         binding.pry
+#         if coupon[:num] >= info[:count]
+#         qty = coupon[:num] - info[:count]
+#         cart[item][:count] = qty
+#       else
+#         qty = info[:count] - coupon[:num]
+#         cart[item][:count] = qty
+#       end
+      
+
+#       else  
+#         return cart
+#       end
+      
+#     end
+#   end
+#   cart.merge(the_discount)
+# end
+
 def apply_coupons(cart, coupons)
   reduced = cart.dup
   coupon_name = nil
   
-  
   cart.each do |item, info|
     coupons.each do |coupon|
       
-      cost = coupon[:cost] 
-      number = coupon[:num]
-      
+      if coupon[:item] == item  && reduced[item][:count] >= coupon[:num]
         
-      if coupon[:item] == item  
-        binding.pry
         coupon_name = "#{item} W/COUPON"
         reduced[item][:count] -= coupon[:num]
-      end
-      
-      if reduced.key?(coupon_name) && reduced[item][:count] >= coupon[:num]
-        reduced[coupon_name][:count] += 1
-      else
-        reduced[coupon_name] = {price: cost, :clearance => info[:clearance], count: 1 }
         
+        if reduced.key?(coupon_name)
+          reduced[coupon_name][:count] += 1
+        else
+          reduced[coupon_name] = {price: coupon[:cost], :clearance => info[:clearance], count: 1 }
+        
+        end
       end
     end
   end
@@ -52,9 +87,72 @@ end
 
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |item, info|
+    if info[:clearance] == true
+      cart[item][:price] = cart[item][:price] - (cart[item][:price] * 0.2)
+    end
+  end
 end
 
+
 def checkout(cart, coupons)
-  # code here
+  prices = []
+  total = 0
+  cart = consolidate_cart(cart)
+  
+    if cart.keys.size == 1 
+      cart = apply_coupons(cart, coupons)
+      if coupons.size == 0 
+        cart = apply_clearance(cart)
+      end
+    end
+    
+    if cart.keys.size > 1 && coupons.size == 1
+      cart = apply_coupons(cart, coupons)
+      cart = apply_clearance(cart)
+      
+    end
+    
+    
+
+  cart.each do |item, info|
+    prices << info[:price]
+    prices.each { |price| total += price }
+    
+  end
+   if total > 100.0
+     total -= (total * 0.1) 
+   end
+   total
 end
+
+
+
+
+
+
+
+
+
+# def checkout(cart, coupons)
+#   prices = []
+  
+  
+#   cart = consolidate_cart(cart)
+#   cart.each do |hash|
+#     hash.each do |item, info|
+#       if coupons.empty? && info[:clearance] == false
+#         prices << info[:price]
+#         total = 0
+#         prices.each { |price| total += price }
+        
+#         return total
+#       end
+#     end
+#   end
+#   # cart = 
+#   # cart = apply_coupons(cart, coupons)
+#   # apply_clearance(cart)
+  
+  
+# end
